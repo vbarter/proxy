8326e8fd82b142558f7bd73591264bd91b1bae18
vbarter <yzcaijunjie@gmail.com>
2024-03-11 16:57:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "openai.ehco-relay.cc";
+    url.host = "api.openai.com/v1";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
